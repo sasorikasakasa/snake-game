3f6f10637ef260d00e0c9e99f08e56c98e32c00c
Takumi Kosaka <kosaka@TakuminoMacBook-Pro.local>
2021-04-15 21:15:50 +0900

    complete app

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({status,onStart,onStop,onRestart}) => {
+  return (
+    <div className="button">
+    {status === "gameover" && <button onClick={onRestart}>gameover</button>}
+    {status === "init" && <button onClick={onStart}>start</button>}
+    {status === "suspended" && <button onClick={onStart}>start</button>}
+    {status === "playing" && <button onClick={onStop}>stop</button>}
+    </div>
+  );
+};
+
+export default Button;
